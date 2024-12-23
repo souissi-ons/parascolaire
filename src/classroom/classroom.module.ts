846defea4185ca10b8cc4d3192588da15a77d168
Aziz Sandid <sandid.aziz123@gmail.com>
2024-12-23 17:48:00 +0100

    module classroom

--- src/classroom/classroom.module.ts
+++ src/classroom/classroom.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClassroomService } from './classroom.service';
 import { ClassroomController } from './classroom.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Classroom } from './entities/classroom.entity';
 
 @Module({
   controllers: [ClassroomController],
   providers: [ClassroomService],
+  imports: [TypeOrmModule.forFeature([Classroom])],
 })
 export class ClassroomModule {}
